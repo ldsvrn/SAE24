1d0859f84169bc52a5b27d95fb09c0da814136d8
ldsvrn <louis2555@orange.fr>
2022-06-22 15:53:27 +0200

    export to csv

--- django/SAE24/temps/views.py
+++ django/SAE24/temps/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Sensors, SensorsData
 from .forms import SensorsForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.forms.models import model_to_dict
+import csv
 
 def liste_sensors(request):
     sensors = Sensors.objects.all()
@@ -36,3 +37,31 @@ def save_modif_sensors(request, id):
         return HttpResponseRedirect("/sensors/liste")
     else:
         return render(request, "sensors/modif.html", {"form": objform, "id": id})
+
+
+def filtre_par_sensor(request, id):
+    data = SensorsData.objects.filter(sensor = id)
+    return render(request, 'data/liste.html', {'data': data})
+
+
+def export_csv(request, id):
+    data = SensorsData.objects.filter(sensor = id)
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="export.csv"'},
+    )
+
+    writer = csv.writer(response)
+    writer.writerow(['timestamp', 'macaddr', 'piece', 'emplacement', 'nom', 'temp'])
+    for i in data:
+        writer.writerow(
+            [i.datetime, 
+            i.sensor.macaddr, 
+            i.sensor.piece,
+            i.sensor.emplacement,
+            i.sensor.nom,
+            i.temp]
+            )
+
+    return response
